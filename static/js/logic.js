d1ae4d275f4aa72937ede14e3267f22d2717bc78
lorelles <74384017+lorelles@users.noreply.github.com>
2021-05-02 19:00:48 -0700

    adding fault lines

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,19 +1,20 @@
 
 // Store our API endpoint inside queryUrl
-const queryUrl = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2018-01-11&endtime=" +
-    "2018-01-12&maxlongitude=-69.52148437&minlongitude=-123.83789062&maxlatitude=48.74894534&minlatitude=25.16517337";
+// const queryUrl = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2018-01-11&endtime=" +
+//     "2018-01-12&maxlongitude=-69.52148437&minlongitude=-123.83789062&maxlatitude=48.74894534&minlatitude=25.16517337";
+// const queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 
 // Perform a GET request to the query URL
-d3.json(queryUrl).then(data => {
-    console.log(data);
-    console.log(d3.extent(data.features.map(d => d.geometry.coordinates)))
-    console.log(d3.extent(data.features.map(d => d.geometry.coordinates[2])))
+// d3.json(queryUrl).then(data => {
+//     console.log(data);
+//     console.log(d3.extent(data.features.map(d => d.geometry.coordinates)))
+//     console.log(d3.extent(data.features.map(d => d.geometry.coordinates[2])))
 
-    // Once we get a response, send the data.features object to the createFeatures function
-    createFeatures(data.features);
-});
+//     // Once we get a response, send the data.features object to the createFeatures function
+//     createFeatures(data.features);
+// });
 
-function createFeatures(earthquakeData) {
+function createFeatures(earthquakeData, platedata) {
 
     // Define a function we want to run once for each feature in the features array
     // Give each feature a popup describing the place and time of the earthquake
@@ -21,29 +22,7 @@ function createFeatures(earthquakeData) {
         layer.bindPopup("<h4>" + "Magnitude/Location:  " + feature.properties.title +
             "</h4><hr><p>" + "Day/Time:  " + new Date(feature.properties.time) + "</p><hr><p>" + "Depth:  " + feature.geometry.coordinates + "</p>");
     }
-
-    // // adding style fuction for markers from Penn State Geography Dept.
-    // function markerStyle(feature) {
-    //     var colorMarker;
-    //     var depth = feature.geometry.coordinates[2];
-
-    //     if (depth > "90") colorMarker = "#800026";
-    //     else if (depth > "70") colorMarker = "#E31A1C";
-    //     else if (depth > "50") colorMarker = "#FC4E2A";
-    //     else if (depth > "30") colorMarker = "#FD8D3C";
-    //     else if (depth > "10") colorMarker = "#FED976";
-    //     else if (depth > "-10") colorMarker = "#FFEDA0";
-    //     else colorMarker = "#FFEDA0";
-
-    //     return {
-    //         "color": colorMarker,
-    //         "weight": 5
-    //     };
-    // }
-    // var markerLayer = L.geoJSON(earthquakeData, {
-    //     style: markerStyle
-    //   });
-
+  
 
     // Create a GeoJSON layer containing the features array on the earthquakeData object
     // Run the onEachFeature function once for each piece of data in the array
@@ -91,49 +70,16 @@ function createFeatures(earthquakeData) {
         }
     });
 
-    const mags = L.geoJSON(earthquakeData, {
+    const mags = L.geoJSON(platedata, {
         onEachFeature: onEachFeature,
-        style: function(feature) {
-            // console.log(feature.geometry.coordinates[2]);
-            var depth = feature.geometry.coordinates[2];
-            if (depth >= 91) {
-              return {
-                color: '#bd0026'
-              }; 
-            }
-            else if (depth >= 71) {
-              return {
-                color: '#e31a1c'
-              };
-            } else if (depth >= 51) {
-              return {
-                color: '#fc4e2a'
-              };
-            } else if (depth >= 31) {
-                return {
-                  color: '#fd8d3c'
-                };
-            } else if (depth >= 11) {
-                    return {
-                      color: '#feb24c'
-                    };
-                } else if (depth >= -10) {
-                    return {
-                      color: '#ffeda0'
-                    };
-            } else {
-              return {
-                color: '#969696'
-              }
-            }
-          },
-        pointToLayer: (feature, latlng) => {
-            return new L.Circle(latlng, {
-                radius: feature.properties.mag * 50000,
-                fillColor: "red",
-                stroke: false
-            });
-        }
+
+        // pointToLayer: (feature, latlng) => {
+        //     return new L.Circle(latlng, {
+        //         radius: feature.properties.mag * 50000,
+        //         fillColor: "red",
+        //         stroke: false
+        //     });
+        // }
     });
 
     // Sending our earthquakes layer to the createMap function
@@ -236,22 +182,22 @@ function createMap(earthquakes, mags) {
 }
 
 
-/* NOTE FOR STEP 2
-/  You can use the javascript Promise.all function to make two d3.json calls,
-/  and your then function will not run until all data has been retreived.
-/
-/ ----------------------------------------
-/  Promise.all(
-/    [
-/        d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"),
-/        d3.json("https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json")
-/    ]
-/  ).then( ([data,platedata]) => {
-/
-/        console.log("earthquakes", data)
-/        console.log("tectonic plates", platedata)
-/
-/    }).catch(e => console.warn(e));
-/
-/ ----------------------------------------*/
+//  NOTE FOR STEP 2
+// /  You can use the javascript Promise.all function to make two d3.json calls,
+// /  and your then function will not run until all data has been retreived.
+// /
+// / ----------------------------------------
+Promise.all(
+    [
+        d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"),
+        d3.json("https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json")
+    ]
+  ).then( ([data, platedata]) => {
+    createFeatures(data.features, platedata.features);
+       console.log("earthquakes", data)
+       console.log("tectonic plates", platedata)
+
+    }).catch(e => console.warn(e));
+
+// / ----------------------------------------
 
