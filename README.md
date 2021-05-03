cb2876b426b9f14364d671df1836121310f095bf
lorelles <74384017+lorelles@users.noreply.github.com>
2021-05-02 20:06:37 -0700

    creating readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # leaflet-challenge
+
+This project utilizes leaflet within html and Javascript in order to visualize earthquake data obtained from the United States Geological Survey (USGS). The maps in this project present earthquakes and tectonic plates based on their longitude and latitude. The size of the data markers represents the magnitude of the earthquake, and the color coordinates with the depth, following the legend key. There is a popup menu that provides additional data when the marker is clicked. The tectonic plates (Displayed in orange) and earthquake data can be clicked on and off to be displayed independently within the control box in the upper right of the map. There are three different map views to choose from: Satelite, Greyscale, and Street view.
+
+To view the webpage, create a config.js file with your personal Mapbox API and add to the "js" file within the "static" folder, then load index.html on your local IDE. The Javascipt for Leaflet is located within "static/js/logic.js." The styling is located in "static/styles.css/css."
+
+The tectonic plate data was originally published in the paper _An updated digital model of plate boundaries_ by Peter Bird, and located on GitGub at: https://github.com/fraxen/tectonicplates, and made available under the Open Data Commons Attribution License: http://opendatacommons.org/licenses/by/1.0/. 
--- static/js/logic.js
+++ static/js/logic.js
@@ -1,19 +1,3 @@
-
-// Store our API endpoint inside queryUrl
-// const queryUrl = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2018-01-11&endtime=" +
-//     "2018-01-12&maxlongitude=-69.52148437&minlongitude=-123.83789062&maxlatitude=48.74894534&minlatitude=25.16517337";
-// const queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
-
-// Perform a GET request to the query URL
-// d3.json(queryUrl).then(data => {
-//     console.log(data);
-//     console.log(d3.extent(data.features.map(d => d.geometry.coordinates)))
-//     console.log(d3.extent(data.features.map(d => d.geometry.coordinates[2])))
-
-//     // Once we get a response, send the data.features object to the createFeatures function
-//     createFeatures(data.features);
-// });
-
 // Define a function we want to run once for each feature in the features array
 function createFeatures(data, platedata) {
 
@@ -21,54 +5,56 @@ function createFeatures(data, platedata) {
     function onEachFeature(feature, layer) {
         layer.bindPopup("<h4>" + "Magnitude/Location:  " + feature.properties.title +
             "</h4><hr><p>" + "Day/Time:  " + new Date(feature.properties.time) + "</p><hr><p>" + "Depth:  " + feature.geometry.coordinates + "</p>");
-    } 
+    }
 
-    // Create a GeoJSON layer containing the features array on the earthquakeData object
-    // Run the onEachFeature function once for each piece of data in the array
+    // Create a GeoJSON layer containing the features array on the earthquake and tectonic objects
+    // Run the onEachFeature function once for each piece of data in the earthquake data array
     const earthquakes = L.geoJSON(data, {
         onEachFeature: onEachFeature,
-        style: function(feature) {
-            // console.log(feature.geometry.coordinates[2]);
+        style: function (feature) {
+            // set color of marker based on depth (Leafletjs.com)
             var depth = feature.geometry.coordinates[2];
             if (depth >= 91) {
-              return {
-                color: '#bd0026'
-              }; 
+                return {
+                    color: '#bd0026'
+                };
             }
             else if (depth >= 71) {
-              return {
-                color: '#e31a1c'
-              };
+                return {
+                    color: '#e31a1c'
+                };
             } else if (depth >= 51) {
-              return {
-                color: '#fc4e2a'
-              };
+                return {
+                    color: '#fc4e2a'
+                };
             } else if (depth >= 31) {
                 return {
-                  color: '#fd8d3c'
+                    color: '#fd8d3c'
                 };
             } else if (depth >= 11) {
-                    return {
-                      color: '#feb24c'
-                    };
-                } else if (depth >= -10) {
-                    return {
-                      color: '#ffeda0'
-                    };
+                return {
+                    color: '#feb24c'
+                };
+            } else if (depth >= -10) {
+                return {
+                    color: '#ffeda0'
+                };
             } else {
-              return {
-                color: '#969696'
-              }
+                return {
+                    color: '#969696'
+                }
             }
-          },
+        },
+        // set marker size
         pointToLayer: (feature, latlng) => {
             return new L.Circle(latlng, {
                 radius: Math.round(feature.properties.mag) * 50000,
-                stroke: true         
+                stroke: true
             });
         }
     });
 
+    // Run the onEachFeature function once for each piece of data in the platedata array
     const plates = L.geoJSON(platedata, {
         onEachFeature: onEachFeature,
         color: "orange",
@@ -79,9 +65,10 @@ function createFeatures(data, platedata) {
     createMap(earthquakes, plates);
 }
 
+// Create the map with our layers
 function createMap(earthquakes, plates) {
 
-    // Define satelite map and street map layers
+    // Define satelite, greyscale, and street map layers
     const satmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
         attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
         tileSize: 512,
@@ -122,7 +109,7 @@ function createMap(earthquakes, plates) {
         "Tectonic Plates": plates
     };
 
-    // Create our map, giving it the satmap and earthquakes layers to display on load
+    // Create our map, giving it the satmap, earthquakes, and plate layers to display on load
     const myMap = L.map("map", {
         center: [
             37.09, -95.71
@@ -130,19 +117,6 @@ function createMap(earthquakes, plates) {
         zoom: 2.5,
         layers: [satmap, earthquakes, plates]
     });
-
-
-    // // adding style fuction for markers from Leafletjs.com
-    // function style(feature) {
-    //     return {
-    //         fillColor: getColor(feature.geometry.coordinates[2]),
-    //         weight: 2,
-    //         opacity: 1,
-    //         color: 'white',
-    //         dashArray: '3',
-    //         fillOpacity: 0.7
-    //     };
-    // }
 
     // Create a layer control, add our overlay layers to it (Leafletjs.com)
     // Pass in our baseMaps and overlayMaps
@@ -160,23 +134,21 @@ function createMap(earthquakes, plates) {
                                 '#969696';
     }
 
-
     // Create a legend to display information about our map
     const legend = L.control({
         position: "bottomright"
     });
+
     // When the layer control is added, insert a div with the class of "legend"
     legend.onAdd = function (myMap) {
         const div = L.DomUtil.create("div", "legend"),
             depths = [-9, 11, 31, 51, 71, 91],
             labels = [];
-
         // loop through our density intervals and generate a label with a colored square for each interval
         for (var i = 0; i < depths.length; i++) {
             div.innerHTML +=
                 '<i style="background:' + getColor(depths[i] + 1) + '"></i> ' +
                 depths[i] + (depths[i + 1] ? '&ndash;' + depths[i + 1] + '<br>' : '+');
-
         }
         return div;
     };
@@ -184,23 +156,15 @@ function createMap(earthquakes, plates) {
     legend.addTo(myMap);
 }
 
-
-//  NOTE FOR STEP 2
-// /  You can use the javascript Promise.all function to make two d3.json calls,
-// /  and your then function will not run until all data has been retreived.
-// /
-// / ----------------------------------------
+// for step 2 use the javascript Promise.all function to make two d3.json calls to retrieve all data before loading
 Promise.all(
     [
         d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"),
         d3.json("https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json")
     ]
-  ).then( ([data, platedata]) => {
+).then(([data, platedata]) => {
     createFeatures(data.features, platedata.features);
-       console.log("earthquakes", data)
-       console.log("tectonic plates", platedata)
+    console.log("earthquakes", data)
+    console.log("tectonic plates", platedata)
 
-    }).catch(e => console.warn(e));
-
-// / ----------------------------------------
-
+}).catch(e => console.warn(e));
